6408c9496f136c8473c5f7979d2249da3606d962
Amir Mehrab <46044502+amirmehrab@users.noreply.github.com>
2019-01-05 13:33:57 +0330

    Add files via upload

---
+++ MSST.py
@@ -0,0 +1,37 @@
+#/bin/python2
+import smtplib
+import time
+import getpass
+import os
+
+os.system("clear")
+print """\n
+    To use this feature, you must activate the Google Account Limit to this section
+		https://myaccount.google.com/data-and-personalization
+		And tick the "Web & App Activity" option """
+
+ti = input("\nEnter Time For Sleeping Script (Secound) >> ")
+print ("\n\tWait until the mail arrives")
+
+
+sender = raw_input("\nEnter Sender Gmail >> ")  
+
+toaddrs = raw_input("\nEnter Target MAil >> ")
+
+message = raw_input("""\nEnter Your Message >>  
+ 
+ """)  
+
+username = raw_input("\nEnter Your Username(Gmail) >> ")
+
+password = getpass.getpass("\nEnter Your Password >> ") 
+            
+time.sleep(ti)
+
+server = smtplib.SMTP("smtp.gmail.com:587") 
+server.starttls() 
+server.login( username , password ) 
+server.sendmail(sender, toaddrs, message) 
+server.quit() 
+
+print "The operation was carried out"
